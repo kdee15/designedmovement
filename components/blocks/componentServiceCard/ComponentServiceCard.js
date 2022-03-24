1bab6080f6e9e224d8625e7c43e9e8468a542c9b
Kirk Daniels <kirkd@ayoba.me>
2022-03-25 01:06:05 +0200

    stuff

--- components/blocks/componentServiceCard/ComponentServiceCard.js
+++ components/blocks/componentServiceCard/ComponentServiceCard.js
@@ -1,13 +1,9 @@
-import { createClient } from "contentful";
 import classes from "./ComponentServiceCard.module.scss";
-import Image from "next/image";
 import React from "react";
 import ReactMarkdown from "react-markdown";
 
 function ComponentServiceCard({ servicesList }) {
   const Services = servicesList.items;
-  // const { description, image, logo, theme } = servicesList.items;
-  console.log("servicesList", servicesList);
   return (
     <div className={`${classes.oServicesListBlock} container`}>
       <div className={`${classes.oRow} row`}>
@@ -17,21 +13,18 @@ function ComponentServiceCard({ servicesList }) {
           <article
             className={`${classes.oCard} col-12 col-md-6`}
             key={Service.sys.id}
-            style={{
-              backgroundImage: `url(http:${Service.fields.image.fields.file.url})`,
-            }}
           >
-            <div className={`container`}>
-              <div className={`${classes.oContentRow} row`}>
-                <div
-                  className={`${classes.oContentBlock} col-12 col-md-6 offset-md-6 col-12 col-lg-7 offset-lg-5`}
-                >
-                  <h2>{Service.fields.title}</h2>
-                  <ReactMarkdown className={`${classes.aText} a-fnt-18f`}>
-                    {Service.fields.copy}
-                  </ReactMarkdown>
-                </div>
-              </div>
+            <div
+              className={classes.mCardBG}
+              style={{
+                backgroundImage: `url(http:${Service.fields.image.fields.file.url})`,
+              }}
+            ></div>
+            <div className={classes.mCardBody}>
+              <h2>{Service.fields.title}</h2>
+              <ReactMarkdown className={`${classes.aText} a-fnt-18f`}>
+                {Service.fields.copy}
+              </ReactMarkdown>
             </div>
           </article>
         ))}
